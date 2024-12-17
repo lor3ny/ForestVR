cff372710428eae6ade5d4413ceda23b1770f8c8
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-12-17 23:27:05 +0000

    Final debugging :)

--- ForestVR/Assets/RockBouncing.cs
+++ ForestVR/Assets/RockBouncing.cs
@@ -6,6 +6,11 @@ public class RockBouncing : MonoBehaviour
 {
 
     public PhysicMaterial bounce;
+    public AudioClip clip;
+
+    public GameObject[] keypoints;
+
+    int bounces = 0;
 
     private void Start()
     {
@@ -23,6 +28,22 @@ public class RockBouncing : MonoBehaviour
     {
         gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         gameObject.GetComponent<Collider>().material = bounce;
+        for (int i = 0; i < keypoints.Length; i++)
+        {
+            keypoints[i].SetActive(false);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Water"))
+        {
+            if (bounces >= 3)
+                return;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+            bounces += 1;
+        }
     }
 
 }
